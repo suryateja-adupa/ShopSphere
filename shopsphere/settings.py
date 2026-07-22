4cc116b87cdc340983a9799dbd552741e99c93d1
surya <suryatejaadupa@gmail.com>
2026-07-22 21:22:09 +0530

    Fix STORAGES

--- shopsphere/settings.py
+++ shopsphere/settings.py
@@ -136,6 +136,9 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
