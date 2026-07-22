55b5c1f539c5cd35690ccc905d70d84774e00e31
surya <suryatejaadupa@gmail.com>
2026-07-22 20:04:53 +0530

    Seed products on deploy

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+python manage.py seed_products
+
+python manage.py collectstatic --no-input
